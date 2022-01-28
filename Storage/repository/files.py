aff8814bf60b131a80594f854a6cd0e4f5d9d25c
vichu <vichu2grand@gmail.com>
2022-01-29 01:28:10 +0530

    used variables for all file directory references

--- Storage/repository/files.py
+++ Storage/repository/files.py
@@ -9,13 +9,16 @@ from fastapi.responses import JSONResponse
 from fastapi.responses import FileResponse
 import zipfile
 
-filePath = "/Users/roviros/Desktop/files_uploaded_cloudwiry/"
+# filePath = os.environ["PATH"] 
+filePath = "/Users/roviros/Desktop/files_uploaded_cloudwiry"
 
 # LOGIC UPLOAD FILE .
 def upload_file(email:str, uploaded_file: UploadFile = File(...)):
     print(email)
     print(email[1:-1])
     email = email[1:-1]
+
+    print(filePath)
     file_location=os.path.join(filePath, f"{email}/{uploaded_file.filename}")
 
     with open(file_location, "wb+") as file_object:
@@ -26,7 +29,7 @@ def upload_file(email:str, uploaded_file: UploadFile = File(...)):
 # LOGIC SHOW ALL FILES OF USER.
 def show_files(email:str):
     try:
-        return os.listdir(f"/Users/roviros/Desktop/files_uploaded_cloudwiry/{email}")
+        return os.listdir(f"{filePath}/{email}")
 
     except:
         raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail = f"This file was not found in our database!")
@@ -38,8 +41,8 @@ def share_file(sender:str,reciever:str,filename:str):
     print(reciever[1:-1])
     print(filename)
     
-    original = f"/Users/roviros/Desktop/files_uploaded_cloudwiry/{sender}/{filename}"
-    target = f"/Users/roviros/Desktop/files_uploaded_cloudwiry/{reciever}/{filename}"
+    original = f"{filePath}/{sender}/{filename}"
+    target = f"{filePath}/{reciever}/{filename}"
     try:
         shutil.copyfile(original, target)
         return JSONResponse(status_code=status.HTTP_200_OK, content=f"File shared successfully to {reciever}")
@@ -50,33 +53,33 @@ def share_file(sender:str,reciever:str,filename:str):
 # LOGIC TO DELETE FILE BY NAME.
 def delete_file(filename:str, email:str):
     try:
-        filePath = f"/Users/roviros/Desktop/files_uploaded_cloudwiry/{email}/{filename}" #here email and the filename will come
-        os.chmod(filePath, 0o777)
-        os.remove(f"{filePath}")
+        fileLocation = f"{filePath}/{email}/{filename}" #here email and the filename will come
+        os.chmod(fileLocation, 0o777)
+        os.remove(f"{fileLocation}")
         return JSONResponse(status_code=status.HTTP_200_OK, content="Deleted file successfully!")
    
     except: 
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail = f"hmmmmm... Something went wrong.")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail = f"Looks like the file \'{filename}\' is not present in your account")
 
 # LOGIC TO RENAME THE FILE USING OLD AND NEW NAMES.
 def rename_file(email:str,old_name:str,new_name:str):
-    sourceFilePath = f"/Users/roviros/Desktop/files_uploaded_cloudwiry/{email}/{old_name}"
-    destinationFilePath = f"/Users/roviros/Desktop/files_uploaded_cloudwiry/{email}/{new_name}"
+    sourceFilePath = f"{filePath}/{email}/{old_name}"
+    destinationFilePath = f"{filePath}/{email}/{new_name}"
 
     try:
         os.rename(sourceFilePath, destinationFilePath)
         return JSONResponse(status_code=status.HTTP_200_OK, content=f"File {old_name} successfully renamed to {new_name}")
     
     except:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"This file name was not found in our database!")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail = f"Looks like the file \'{old_name}\' is not present in your account")
 
 
 # LOGIC TO DOWNLOAD FILE BY NAME.
 def download_file(email:str, filename:str):
 
     try:
-        filePath = f"/Users/roviros/Desktop/files_uploaded_cloudwiry/{email}/{filename}"
-        return filePath
+        fileLocation = f"{filePath}/{email}/{filename}"
+        return fileLocation
     
     except:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = "This file name was not found in our database!")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"{filename} was not found in our database!")
