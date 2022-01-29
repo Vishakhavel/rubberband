03dbeb62bdcd90dc24b527e2744013a02baf0767
vichu <vichu2grand@gmail.com>
2022-01-29 17:02:23 +0530

    added manual sudo command

--- Storage/routers/user.py
+++ Storage/routers/user.py
@@ -19,9 +19,10 @@ get_db = database.get_db
 
 #old EFS file path
 # filePath = "/home/ec2-user/efs-mount-point/files"
+os.system("sudo mkdir /ef/files/checker_folder")
 
 #new EFS file path
-filePath = "efs/files"
+filePath = "/efs/files"
 
 
 
